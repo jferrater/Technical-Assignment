690caed18e8962ba74fc6c122956debf12485da5
Joffry Ferrater <joffry.ferrater@gmail.com>
2017-01-18 11:54:45 +0100

    update script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar radiograph-service/target/radiograph-service-0.0.1-SNAPSHOT.jar &
+java -jar radiograph-service/radiograph-service-0.0.1-SNAPSHOT.jar &
 cd UI
 npm install &
 npm start &
